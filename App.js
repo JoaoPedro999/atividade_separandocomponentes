6917da751de4ae9d252d6982ad8610396d7cf656
João Pedro Araújo Teixeira <116459774+JoaoPedro999@users.noreply.github.com>
2024-02-28 10:01:53 -0300

    commit

--- App.js
+++ App.js
@@ -9,7 +9,9 @@ import {
   ImageBackground,
 } from "react-native";
 
+/*  estilos */
 import styles from "./src/assets/styles/component_style";
+/* componentes */
 import Component1 from "./src/components/component1";
 import Component2 from "./src/components/component2";
 import Component3 from "./src/components/component3";
@@ -17,9 +19,16 @@ import Component3 from "./src/components/component3";
 export default function App() {
   return (
     <View style={styles.container}>
+
       <StatusBar style="auto" />
+
+   {/*  cabecalho */}
       <Component1/>
+
+    {/*   corpo */}
       <Component2/>
+
+     {/*  rodape */}
       <Component3/>
 
     </View>
--- src/assets/styles/component_style.js
+++ src/assets/styles/component_style.js
@@ -1,31 +1,23 @@
+import {StyleSheet} from 'react-native';
 
-import {StyleSheet} from 'react-native' ; 
+const styles = StyleSheet.create({
+    cabecalho: {
+        backgroundColor: "gray",
+        fontWeight: "bold",
+        width: "100%",
+        height: 20,
+    },
+    rodape: {
+        backgroundColor: "gray",
+        fontWeight: "bold",
+        width: "100%",
+        height: 20,
+    },
+    body: {
+        backgroundColor: "#fff",
+        justifyContent: "center", 
+        alignItems: "center", 
+    },
+});
 
-    const styles = StyleSheet.create({
-        cabecalho: {
-            backgroundColor: "gray",
-            justifyContent: "top",
-            alignItems: "top",
-            fontWeight: "bold",
-            width: "100%",
-            height: "20",
-        },
-        rodape: {
-            backgroundColor: "gray",
-            justifyContent: "top",
-            alignItems: "top",
-            fontWeight: "bold",
-            width: "100%",
-            height: "20",
-        },
-        body: {
-            backgroundColor: "#fff",
-            justifyContent: "center",
-            alignItems: "center",
-        },
-    });
-
-    export default styles;
-
-
-
+export default styles;
