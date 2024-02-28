6287bc895658400715a1227cfdc658cdc711016f
João Pedro Araújo Teixeira <116459774+JoaoPedro999@users.noreply.github.com>
2024-02-28 10:31:00 -0300

    commit - cabeçalho posicionado

--- src/styles/component_style.js
+++ src/styles/component_style.js
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     width: "100%",
     height: 50,
-    bottom: "45%",
+    bottom: "42.5%",
   },
   rodape: {
     backgroundColor: "gray",
